27a7322f21e0dd635c59aa5916cb4c50b51af431
Antonio Fernández <628492+antoniofv@users.noreply.github.com>
2023-02-17 15:00:23 +0100

    Initial Commit

---
+++ iOS-Recycling/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-Recycling
+//
+//  Created by Antonio Fernandez Vega on 17/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Recycling/iOS_RecyclingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_RecyclingApp.swift
+//  iOS-Recycling
+//
+//  Created by Antonio Fernandez Vega on 17/2/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_RecyclingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
