e16e9bcd8351a7d7d7566ee93f4b8a0b6722dfaf
Antonio Fernández <628492+antoniofv@users.noreply.github.com>
2023-04-15 01:13:27 +0200

    Add extra view as a tap dismissal area when editing an item on the grocery list

--- iOS-Recycling/Views/GroceryListView.swift
+++ iOS-Recycling/Views/GroceryListView.swift
@@ -9,12 +9,28 @@ import Combine
 import SwiftUI
 
 
+struct BoundsPreferenceKey: PreferenceKey {
+    typealias Value = [Anchor<CGRect>]
+
+    static var defaultValue: Value = []
+
+    static func reduce(
+        value: inout Value,
+        nextValue: () -> Value
+    ) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
 struct GroceryListView: View {
 
     @StateObject private var groceryListViewModel: GroceryListViewModel
     @FocusState private var focusedItem: UUID?
 
     @State var timer: Timer?
+
+    @Environment(\.defaultMinListRowHeight) var minRowHeight
+    @State private var listHeight = CGFloat.infinity
 
 
     init(viewModel: GroceryListViewModel) {
@@ -24,92 +40,147 @@ struct GroceryListView: View {
 
     var body: some View {
         NavigationStack {
-            List() {
-                Section {
-                    ForEach(groceryListViewModel.groceries, id: \.id) {
-                        GroceryListCell(
-                            groceryListItem:
-                                $0,
-                            focusState: $focusedItem,
-                            isEditing: focusedItem == $0.id
-                        )
-                        .onSubmit {
-                            self.focusedItem = nil
-                            self.timer?.invalidate()
-                            self.groceryListViewModel.save()
-                        }
-                        .onChange(of: $0.isChecked) { _ in
-                            // Remove focus from selected element if a checkbox is tapped.
-                            self.focusedItem = nil
-                        }
-                        .onReceive($0.objectWillChange) {
-                            // Wait before committing changes, to prevent unnecessary
-                            // writes to the database if more changes happen in a short time.
-                            self.timer?.invalidate()
+            GeometryReader { proxy in
+                List() {
+                    Section {
+                        ForEach(groceryListViewModel.groceries, id: \.id) { item in
+                            GroceryListCell(
+                                groceryListItem:
+                                    item,
+                                focusState: $focusedItem,
+                                isEditing: focusedItem == item.id
+                            )
+                            .onSubmit {
+//                                focusedItem = nil
+                                timer?.invalidate()
+                                groceryListViewModel.save()
+                                addItem()
+                            }
+                            .onChange(of: item.isChecked) { _ in
+                                // Remove focus from selected element if a checkbox is tapped.
+                                focusedItem = nil
+                            }
+                            .onReceive(item.objectWillChange) {
+                                // Wait before committing changes, to prevent unnecessary
+                                // writes to the database if more changes happen in a short time.
+                                timer?.invalidate()
 
-                            self.timer = Timer.scheduledTimer(
-                                withTimeInterval: 1,
-                                repeats: false,
-                                block: { timer in
-                                    self.groceryListViewModel.save()
-                                    self.timer = nil
+                                timer = Timer.scheduledTimer(
+                                    withTimeInterval: 1,
+                                    repeats: false
+                                ) { _ in
+                                    groceryListViewModel.save()
+                                    timer = nil
                                 }
+                            }
+                        }
+                        .onDelete { indexSet in
+                            focusedItem = nil
+                            groceryListViewModel.removeItem(atOffsets: indexSet)
+                        }
+                        .onMove { from, to in
+                            focusedItem = nil
+                            groceryListViewModel.moveItem(
+                                fromOffsets: from,
+                                toOffset: to
                             )
                         }
+                        .listRowInsets(EdgeInsets())
+                        .alignmentGuide(.listRowSeparatorLeading) { $0[.leading] }
+                        .anchorPreference(key: BoundsPreferenceKey.self, value: .bounds) { [$0] }
+                        .listRowSeparator(.visible)
 
-                    }
-                    .onDelete { indexSet in
-                        self.focusedItem = nil
-                        self.groceryListViewModel.removeItem(atOffsets: indexSet)
-                    }
-                    .onMove { from, to in
-                        self.focusedItem = nil
-                        self.groceryListViewModel.moveItem(
-                            fromOffsets: from,
-                            toOffset: to
-                        )
-                    }
-                    .listRowInsets(EdgeInsets())
-                    .alignmentGuide(.listRowSeparatorLeading) { $0[.leading] }
-                }
-            }
-            .listStyle(.plain)
-            .listRowSeparator(.visible)
-            .navigationBarTitle(Text("Groceries"))
-            .toolbarBackground(Color.green, for: .navigationBar)
-            .toolbarBackground(.visible, for: .navigationBar)
-            .toolbarColorScheme(.dark, for: .navigationBar)
-            .background(.gray.opacity(0.15))
-            .scrollContentBackground(.hidden)
-            .toolbar {
-                ToolbarItem {
-                    Button(action: {
-                        withAnimation {
-                            addItem()
+                        // FIXME: Adds a view to dismiss the keyboard by tapping on the empty space on the list.
+                        if focusedItem != nil
+                            && !groceryListViewModel.groceries.isEmpty
+                        {
+                            dismissalArea(proxy: proxy)
                         }
-                    }) {
-                        Image(systemName: "plus")
-                            .foregroundColor(.white)
                     }
-                    .accessibilityIdentifier("addButton")
-                    .accessibilityHint(Text("Add a grocery item to the list"))
+
                 }
+                .listStyle(.grouped)
+                .navigationBarTitle(Text("Groceries"))
+                .toolbarBackground(Color.green, for: .navigationBar)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarColorScheme(.dark, for: .navigationBar)
+                .background(Color.gray.opacity(0.15))
+                .scrollContentBackground(.hidden)
+                .toolbar {
+                    ToolbarItem {
+                        Button(action: {
+                            withAnimation {
+                                addItem()
+                            }
+                        }) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                        }
+                        .accessibilityIdentifier("addButton")
+                        .accessibilityHint(Text("Add a grocery item to the list"))
+                    }
+                }
+                .overlay(Group {
+                    // Empty list view
+                    if groceryListViewModel.groceries.isEmpty {
+                        Text("Add some items to the list!")
+                            .accessibilityIdentifier("emptyListText")
+                            .font(.title2)
+                            .foregroundColor(Color(white: 0.4))
+                    }
+                })
+                .backgroundPreferenceValue(BoundsPreferenceKey.self) { rects in
+                    GeometryReader { gr in
+                        self.updateListFrame(gp: gr, anchors: rects)
+                    }
+                }
+//                .gesture(
+//                    SpatialTapGesture(coordinateSpace: .local).onEnded { event in
+//                        print("GESTURATOR")
+//                        // Remove focus only when the tap location is beyond the list content.
+//                        if (focusedItem != nil && event.location.y > listHeight) {
+//                            print("Unfocus item!")
+//                            focusedItem = nil
+//                        }
+//                    },
+//                    including: .subviews
+//                )
             }
-            .overlay(Group {
-                // Empty list view
-                if self.groceryListViewModel.groceries.isEmpty {
-                    Text("Add some items to the list!")
-                        .accessibilityIdentifier("emptyListText")
-                        .font(.title2)
-                        .foregroundColor(Color(white: 0.4))
-                }
-            })
         }
     }
 
+    private func dismissalArea(proxy: GeometryProxy) -> some View {
+        ZStack {
+            Color.clear
+        }
+        .id("dismissal-area")
+        .frame(height: max(0, proxy.size.height - listHeight - 30))
+        .listRowInsets(.zero)
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+        .allowsHitTesting(true)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            focusedItem = nil
+        }
+    }
+
+}
+
+
+extension GroceryListView {
+
     private func addItem() {
-        let newItem = self.groceryListViewModel.createItem()
-        self.focusedItem = newItem.id
+        let newItem = groceryListViewModel.createItem()
+        focusedItem = newItem.id
+    }
+
+    private func updateListFrame(gp: GeometryProxy, anchors: [Anchor<CGRect>]) -> some View {
+        let contentHeight = anchors.reduce(CGFloat.zero) { $0 + gp[$1].size.height }
+        DispatchQueue.main.async {
+            self.listHeight = contentHeight
+        }
+        return Color.clear
     }
 
 }
